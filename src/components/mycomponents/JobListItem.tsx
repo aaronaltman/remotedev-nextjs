960b30e37e0576977170aed5d3b64d9e20d5d196
aka <aaronkaltman@gmail.com>
2024-01-02 23:02:32 -0500

    props

--- src/components/mycomponents/JobListItem.tsx
+++ src/components/mycomponents/JobListItem.tsx
@@ -1,10 +1,28 @@
 import React from "react";
 
-export default function JobListItem() {
+export type JobListItem = {
+  id: string;
+  title: string;
+  company: string;
+  date: string;
+  badgeLetters: string;
+  relevanceScore: number;
+  daysAgo: number;
+};
+
+export type JobListItemProps = {
+  job: JobListItem;
+};
+
+export default function JobListItem({ JobListItem }: JobListItemProps) {
   return (
     <div>
       <section>
-        <p>hello world</p>
+        <li>
+          <p>{JobListItem.badge}</p>
+          <h3>{JobListItem.title}</h3>
+          <p>{JobListItem.company}</p>
+        </li>
       </section>
     </div>
   );
